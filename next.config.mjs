f1d7fa78ac8327457265f6a3bf147dc2791200a3
Medo <mohammed.yuossry@gmail.com>
2024-02-08 10:22:23 +0200

    undo static export

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
-
   images: {
-    loader: "custom",
-    loaderFile: "./my-loader.js",
     remotePatterns: [
       {
         protocol: "https",
