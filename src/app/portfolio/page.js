7daecbaa9aa9923096889c1ace1cde3a44a4cfbf
Nyctophilus <mohammed.yuossry@gmail.com>
2024-06-28 12:42:08 +0300

    undo sadas

--- src/app/portfolio/page.js
+++ src/app/portfolio/page.js
@@ -2,7 +2,6 @@ import { getProjects } from "@/actions";
 import PortfolioPage from "./PortfolioPage";
 
 const Portfolio = async () => {
-  // sasd
   const projects = await getProjects();
   return <PortfolioPage projects={projects} />;
 };
